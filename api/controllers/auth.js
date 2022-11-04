9af88a2b2ebf86b0e0f91dd16f5cca520523d31d
Khang <khangb1910234@student.ctu.edu.vn>
2022-11-04 23:28:25 +0700

    first commit

---
+++ api/controllers/auth.js
@@ -0,0 +1,37 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs"
+import { createError } from "../utils/error.js";
+
+export const register = async (req, res, next)=>{
+    try {
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
+       const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hash,
+       })
+
+       await newUser.save()
+       res.status(200).send("Người dùng đã được tạo.");
+    }catch(err){
+        next(err);
+    }
+};
+
+export const login = async (req, res, next)=>{
+    try {
+       const user = await User.findOne({username: req.body.username})
+       if(!user) return next(createError(404, "Không tìm thấy người dùng!"));
+       
+       const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
+       if(!isPasswordCorrect) 
+        return next(createError(400, "Tài khoản hoặc mật khẩu sai!"));
+
+        const {password, isAdmin, ...otherDetails} = user._doc;
+       res.status(200).json({...otherDetails});
+    }catch(err){
+        next(err);
+    }
+};
